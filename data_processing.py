6d0ee0fa52e184320c8147d81658ee41d42248aa
sleocho <121847819+sleocho@users.noreply.github.com>
2023-10-27 16:56:54 +0900

    sort

--- data_processing.py
+++ data_processing.py
@@ -1,4 +1,5 @@
 import json
+import natsort
 import pandas as pd
 import os
 from datetime import datetime
@@ -24,27 +25,48 @@ class KRX:
         json_files = defaultdict(list)
         date_format = "%Y%m%d%H%M%S"
         result = dict()
+       # a = os.listdir(directory_path)
+        # print(natsort.natsorted(os.listdir(directory_path)))
         # 디렉토리 내의 모든 파일에 대해 반복
         for root, _, files in os.walk(directory_path):
+          #  print(natsort.natsorted(root))
             for file in files:
                 if file.endswith('.json'):  # JSON 파일만 처리
                     filename = os.path.join(root, file)
+                    
                     key = os.path.basename(root)  # 경로에서 디렉토리 이름을 추출하여 key로 사용
                     json_files[root].append(filename)
+
+          ###############################          
+        for i in json_files.keys():
+            file_list = natsort.natsorted(os.listdir(i))   # TODO 최신순으로 재정렬하기
+            
+          #  for folder in folder_list:
+               # file_list = get_folder_files()
+            ticker_list = list()
+            file_name_list = list()
+            for data in file_list:
+                date_time = filename.split("_")[-1].split(".json")[0]
+                print(filename)
+                
+                ticker = '_'.join(filename.split('_')[:-1])
+                if ticker in ticker_list:
+                    continue
+                ticker_list.append(ticker)
+                file_name_list.append(f'{ticker}_{date_time}.json')
+        print(file_name_list)
+            ###################################
 
         # 각 디렉토리 경로마다 가장 최근 파일 선택
         latest_json_data = {}
         for key, files in json_files.items():
-            # print( key, files )
             latest_file = max(files, key=os.path.getctime)  # 가장 최근 생성일을 기준으로 선택
             latest_json_data[key] = latest_file
 
         # latest_json_data에는 경로별로 가장 최근에 생성된 JSON 파일이 포함
-        for key, filename in latest_json_data.items():       
-            print(key, filename)     
+        for key, filename in latest_json_data.items():        
             file_name = filename.split("_")[-1].split(".json")[0]
             
-            # HOZI BEGIN
             leaf = filename.split( '.' )[ 0 ].strip()
             group_id, index_id, dummy, timestamp = list( map( str.strip, leaf.split( '_' ) ) )
             asof = timestamp[ :8 ]
@@ -79,11 +101,8 @@ class KRX:
             asof, index_id = k
             asdf = pd.DataFrame( v )
             dest = os.path.join( loot, '{}-{}.csv'.format( index_id, asof ) )
-            asdf.to_csv( dest, index=False, encoding="euc-kr")
+            # asdf.to_csv( dest, index=False, encoding="euc-kr")
             
-        # df = pd.DataFrame(json_data)
-        # # 데이터 프레임을 CSV 파일로 저장
-        # df.to_csv(Config.CSV_PATH, index=False, encoding="euc-kr")
 
 krx = KRX()
 json_data = krx.read_all_json_files_in_directory(Config.JSON_PATH + "/MKD03040101")
